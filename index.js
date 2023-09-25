54098bd3cd1afb39569cf44148cdce2837f421b5
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-09-25 14:22:27 +0600

    Added data in MongoDB by Node Js

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = 3002;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// create product schema
+const productsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price : {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+// create product model
+const Product = mongoose.model("Products", productsSchema);
 
 const connectDB = async () => {
     try {
@@ -28,3 +53,33 @@ app.listen(port, async  ()=>{
 app.get("/",(req,res) => {
     res.send("welcome to home page");
 });
+
+app.post("/products", async (req, res) => {
+    try {
+        
+        //const newProduct = new Product ({
+            //title : req.body.title,
+            //price : req.body.price,
+            //description : req.body.description,
+        const produtData = await Product.insertMany([
+            {
+                "title" : "iphone 5",
+                price: 250,
+                description: "beautiful phone"
+            },
+            {
+                "title" : "iphone 4",
+                price: 200,
+                description: "beautiful phone"
+            }
+
+        ]);
+       const productData = await newProduct.save();
+
+        res.status(201).send({productData});
+
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
